c2b12344243bd977008705e80ad849704e5733e8
Matias <71100012+matiasFS@users.noreply.github.com>
2022-03-08 15:26:54 -0300

    Modificacion en las urls

--- ProyectoWeb/ProyectoWebApp/urls.py
+++ ProyectoWeb/ProyectoWebApp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name="Home"),
     path('services', views.serivces, name="Services"),
     path('store', views.store, name="Store"),
-    path('blogg', views.blog, name="Blog"),
+    path('blog', views.blog, name="Blog"),
     path('contact', views.contact, name="Contact"),
 
 ]
--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home")
+    return render(request, "ProyectoWebApp/home.html")
 
 def serivces(request):
-    return HttpResponse("Services")
+    return render(request, "ProyectoWebApp/services.html")
 
 def store(request):
-    return HttpResponse("Store")
+    return render(request, "ProyectoWebApp/store.html")
 
 def blog(request):
-    return HttpResponse("Blog")
+    return render(request, "ProyectoWebApp/blog.html")
 
 def contact(request):
-    return HttpResponse("Contact")
+    return render(request, "ProyectoWebApp/contact.html")
