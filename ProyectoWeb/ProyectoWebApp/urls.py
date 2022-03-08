90a1aa5f49efeb0be78e1bef5ee805787ea03c1a
Matias <71100012+matiasFS@users.noreply.github.com>
2022-03-08 16:32:36 -0300

    Creacion tabla  servicios y url para imagenes

--- ProyectoWeb/ProyectoWebApp/urls.py
+++ ProyectoWeb/ProyectoWebApp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from ProyectoWebApp import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="Home"),
@@ -11,3 +12,5 @@ urlpatterns = [
     path('contact', views.contact, name="Contact"),
 
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
