8873760def444c4bcd3cd74421a6cd67201a5a1f
Matias <71100012+matiasFS@users.noreply.github.com>
2022-03-25 16:55:41 -0300

    Login, Logout and Register

--- ProyectoWeb/authentication/urls.py
+++ ProyectoWeb/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, log_out, log_in
 
 urlpatterns = [
     path('', RegisterView.as_view(), name="authentication"),
+    path('logout', log_out, name="logout"),
+    path('login', log_in, name="login"),
 
 ]
--- ProyectoWeb/authentication/views.py
+++ ProyectoWeb/authentication/views.py
@@ -1,15 +1,17 @@
-from atexit import register
+from email import message
 from django.shortcuts import redirect, render
 from django.views import View
-from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth import login 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 # Create your views here.
+
+
 class RegisterView(View):
     def get(self, request):
         form = UserCreationForm()
-        return render(request,"register/register.html",{"form":form})
+        return render(request, "register/register.html", {"form": form})
 
     def post(self, request):
         form = UserCreationForm(request.POST)
@@ -21,6 +23,28 @@ class RegisterView(View):
         else:
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
-            return render(request,"register/register.html",{"form":form})
-            
+            return render(request, "register/register.html", {"form": form})
 
+
+def log_out(request):
+    logout(request)
+    return redirect('Home')
+
+
+def log_in(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data.get("username")
+            pass_word = form.cleaned_data.get("password")
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                return redirect('Home')
+            else:
+                messages.error(request, "User not valid")
+        else:
+            messages.error(request, "Incorrect information")
+    
+    form = AuthenticationForm()   
+    return render(request, "login/login.html", {"form": form})
