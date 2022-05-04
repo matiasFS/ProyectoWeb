258e9029383f01cb6f2c9107765493061e7b79fa
Matias <71100012+matiasFS@users.noreply.github.com>
2022-05-04 15:41:02 -0300

    Arreglos para que funcione el carrito

--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse
-
+from cart.cart import Cart
 # Create your views here.
 def home(request):
+    cart = Cart(request)
     return render(request, "ProyectoWebApp/home.html")
 
 
--- ProyectoWeb/cart/cart.py
+++ ProyectoWeb/cart/cart.py
@@ -1,11 +1,11 @@
 class Cart:
     def __init__(self, request):
-    #    ''' self.request = request
-     #    self.session = request.session
-    #     cart = self.session.get("cart")
-    # #     if not cart:
-    #         cart = self.session["cart"] = {}
-        # else:
+        self.request = request
+        self.session = request.session
+        cart = self.session.get("cart")
+        if not cart:
+            cart = self.session["cart"] = {}
+  
         self.cart = cart
 
     def add_product(self, product):
