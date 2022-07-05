8f6ac1f41f33d1c6f622a8386e2dd36276807d80
Matias Silvestri <71100012+matiasFS@users.noreply.github.com>
2022-07-05 14:19:37 -0300

    Procesar pedido

--- ProyectoWeb/pedidos/views.py
+++ ProyectoWeb/pedidos/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from pedidos.models import NotaPedido, Pedido
+from django.contrib import messages
 
 # Create your views here.
+@login_required(login_url = "/authentication/login")
+def procesar_pedido(request):
+    pedido = Pedido.objects.create(user = request.user)
+    carro = Carro(request)
+    notaPedido = list()
+    for key, value in carro.carro.items():
+        notaPedido.append(NotaPedido(
+            product_id = key,
+            quantity = value["quantity"],
+            user = request.user,
+            pedido = pedido
+        ))
+    NotaPedido.objects.bulk_create(notaPedido)
+
+    send_mail(
+       
+    )
+    messages.success(request, "El pedido se ha creado correctamente")
+
+    return redirect("../store")
